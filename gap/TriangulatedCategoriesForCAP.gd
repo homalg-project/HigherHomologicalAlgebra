fb9f7ba1650216f46aebb2c2c71224be77a023bd
Kamal Saleh <ksaleh87@yahoo.com>
2016-07-21 18:29:10 +0200

    deleting some files

--- gap/TriangulatedCategoriesForCAP.gd
+++
@@ -1,9 +0,0 @@
-#
-# TriangulatedCategoriesForCAP: frame work for triangulated categories
-#
-# Declarations
-#
-
-#! @Description
-#!   Insert documentation for you function here
-DeclareGlobalFunction( "TriangulatedCategoriesForCAP_Example" );
