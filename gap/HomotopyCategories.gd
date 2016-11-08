87e4807d70def533f9ddf26889ba1be8bb3619cd
Kamal Saleh <ksaleh87@yahoo.com>
2016-11-08 18:23:11 +0100

    deleting default functions

--- gap/HomotopyCategories.gd
+++
@@ -1,9 +0,0 @@
-#
-# HomotopyCategories: Package to create homotopy categories of an additive category
-#
-# Declarations
-#
-
-#! @Description
-#!   Insert documentation for you function here
-DeclareGlobalFunction( "HomotopyCategories_Example" );
