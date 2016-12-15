8972cd02df92b6a0cca01b57498af582079d18a6
Kamal Saleh <ksaleh87@yahoo.com>
2016-12-15 13:55:12 +0100

    Two new files are added complexes.gi and complexes.gd They will contain all methods and operations related to chain or cochain complexes.
    The file complexes.gd contains now the following declarations
    
    DeclareCategory( "IsChainOrCochainComplex", IsCapCategoryObject );
    
    DeclareCategory( "IsChainComplex", IsChainOrCochainComplex );
    
    DeclareCategory( "IsCochainComplex", IsChainOrCochainComplex );
    
    DeclareCategory( "IsFiniteChainComplex", IsChainComplex );
    
    DeclareCategory( "IsFiniteCochainComplex", IsCochainComplex );
    
    DeclareOperation( "ChainComplex", [ IsCapCategory, IsZList, IsBool ] );
    
    DeclareOperation( "ChainComplex", [ IsCapCategory, IsZList ] );
    
    DeclareOperation( "CochainComplex", [ IsCapCategory, IsZList, IsBool ] );
    
    DeclareOperation( "CochainComplex", [ IsCapCategory, IsZList ] );
    
    DeclareOperation( "FiniteChainComplex", [ IsDenseList, IsInt ] );
    
    DeclareOperation( "FiniteCochainComplex", [ IsDenseList, IsInt ] );
    
    DeclareOperation( "StalkChainComplex", [ IsCapCategoryObject ] );
    
    DeclareOperation( "StalkCochainComplex", [ IsCapCategoryObject ] );
    
    DeclareOperation( "ChainComplexWithInductiveSides", [ IsCapCategoryMorphism, IsFunction, IsFunction ] );
    
    DeclareOperation( "CochainComplexWithInductiveSides", [ IsCapCategoryMorphism, IsFunction, IsFunction ] );
    
    DeclareOperation( "ChainComplexWithInductiveNegativeSide", [ IsCapCategoryMorphism, IsFunction ] );
    
    DeclareOperation( "ChainComplexWithInductivePositiveSide", [ IsCapCategoryMorphism, IsFunction ] );
    
    DeclareOperation( "CochainComplexWithInductiveNegativeSide", [ IsCapCategoryMorphism, IsFunction ] );
    
    DeclareOperation( "CochainComplexWithInductivePositiveSide", [ IsCapCategoryMorphism, IsFunction ] );
    
    DeclareAttribute( "Differentials", IsChainOrCochainComplex );
    
    DeclareAttribute( "Objects", IsChainOrCochainComplex );
    
    DeclareOperation( "\[\]", [ IsChainOrCochainComplex, IsInt ] );
    
    DeclareOperation( "\^", [ IsChainOrCochainComplex, IsInt ] );
    
    DeclareOperation( "DefectOfExactness", [ IsChainOrCochainComplex, IsInt ] );
    
    DeclareOperation( "IsExactInIndex", [ IsChainOrCochainComplex, IsInt ] );
    
    DeclareOperation( "SetUpperBound", [ IsChainOrCochainComplex, IsInt ] );
    
    DeclareOperation( "SetLowerBound", [ IsChainOrCochainComplex, IsInt ] );
    
    DeclareOperation( "HasActiveUpperBound", [ IsChainOrCochainComplex ] );
    
    DeclareOperation( "HasActiveLowerBound", [ IsChainOrCochainComplex ] );
    
    DeclareOperation( "ActiveUpperBound", [ IsChainOrCochainComplex ] );
    
    DeclareOperation( "ActiveLowerBound", [ IsChainOrCochainComplex ] );
    
    DeclareOperation( "Display", [ IsChainOrCochainComplex, IsInt, IsInt ] );

---
+++ lib/complexes.gd
@@ -0,0 +1,98 @@
+###############################################
+##
+##              CAP package
+##
+##              Kamal Saleh 2016
+##
+###############################################
+
+#########################################
+#
+# Categories and filters
+#
+#########################################
+
+DeclareCategory( "IsChainOrCochainComplex", IsCapCategoryObject );
+
+DeclareCategory( "IsChainComplex", IsChainOrCochainComplex );
+
+DeclareCategory( "IsCochainComplex", IsChainOrCochainComplex );
+
+DeclareCategory( "IsFiniteChainComplex", IsChainComplex );
+
+DeclareCategory( "IsFiniteCochainComplex", IsCochainComplex );
+
+#########################################
+#
+#  Constructors of (Co)chain complexes 
+#
+#########################################
+
+DeclareOperation( "ChainComplex", [ IsCapCategory, IsZList, IsBool ] );
+
+DeclareOperation( "ChainComplex", [ IsCapCategory, IsZList ] );
+
+DeclareOperation( "CochainComplex", [ IsCapCategory, IsZList, IsBool ] );
+
+DeclareOperation( "CochainComplex", [ IsCapCategory, IsZList ] );
+
+DeclareOperation( "FiniteChainComplex", [ IsDenseList, IsInt ] );
+
+DeclareOperation( "FiniteCochainComplex", [ IsDenseList, IsInt ] );
+
+DeclareOperation( "StalkChainComplex", [ IsCapCategoryObject ] );
+
+DeclareOperation( "StalkCochainComplex", [ IsCapCategoryObject ] );
+
+DeclareOperation( "ChainComplexWithInductiveSides", [ IsCapCategoryMorphism, IsFunction, IsFunction ] );
+
+DeclareOperation( "CochainComplexWithInductiveSides", [ IsCapCategoryMorphism, IsFunction, IsFunction ] );
+
+DeclareOperation( "ChainComplexWithInductiveNegativeSide", [ IsCapCategoryMorphism, IsFunction ] );
+
+DeclareOperation( "ChainComplexWithInductivePositiveSide", [ IsCapCategoryMorphism, IsFunction ] );
+
+DeclareOperation( "CochainComplexWithInductiveNegativeSide", [ IsCapCategoryMorphism, IsFunction ] );
+
+DeclareOperation( "CochainComplexWithInductivePositiveSide", [ IsCapCategoryMorphism, IsFunction ] );
+
+#########################################
+#
+# Attributes of (co)chain complexes
+#
+#########################################
+
+
+DeclareAttribute( "Differentials", IsChainOrCochainComplex );
+
+DeclareAttribute( "Objects", IsChainOrCochainComplex );
+
+#########################################
+#
+# operations derived from Attributes
+#
+#########################################
+
+DeclareOperation( "\[\]", [ IsChainOrCochainComplex, IsInt ] );
+
+DeclareOperation( "\^", [ IsChainOrCochainComplex, IsInt ] );
+
+DeclareOperation( "DefectOfExactness", [ IsChainOrCochainComplex, IsInt ] );
+
+DeclareOperation( "IsExactInIndex", [ IsChainOrCochainComplex, IsInt ] );
+
+DeclareOperation( "SetUpperBound", [ IsChainOrCochainComplex, IsInt ] );
+
+DeclareOperation( "SetLowerBound", [ IsChainOrCochainComplex, IsInt ] );
+
+DeclareOperation( "HasActiveUpperBound", [ IsChainOrCochainComplex ] );
+
+DeclareOperation( "HasActiveLowerBound", [ IsChainOrCochainComplex ] );
+
+DeclareOperation( "ActiveUpperBound", [ IsChainOrCochainComplex ] );
+
+DeclareOperation( "ActiveLowerBound", [ IsChainOrCochainComplex ] );
+
+DeclareOperation( "Display", [ IsChainOrCochainComplex, IsInt, IsInt ] );
+
+
