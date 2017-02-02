c91c9edb13d4f4f0fcc0c993f4a996ca78533c3c
Kamal Saleh <ksaleh87@yahoo.com>
2017-02-02 11:48:52 +0100

    Now the user can add two methods to any category
       - EpimorphismFromProjectiveObject
       - MonomorphismInInjectiveObject

--- lib/resolutions.gd
+++ lib/resolutions.gd
@@ -1,1 +1,36 @@
+###############################################
+# resolutions.gd             complex package
 #
+# Feb. 2017
+###############################################
+
+DeclareOperationWithCache( "EpimorphismFromProjectiveObject", [ IsCapCategoryObject  ] );
+
+DeclareOperation( "AddEpimorphismFromProjectiveObject",
+                   [ IsCapCategory, IsFunction, IsInt ] );
+
+DeclareOperation( "AddEpimorphismFromProjectiveObject",
+                   [ IsCapCategory, IsFunction ] );
+
+
+DeclareOperation( "AddEpimorphismFromProjectiveObject",
+                   [ IsCapCategory, IsList, IsInt ] );
+
+DeclareOperation( "AddEpimorphismFromProjectiveObject",
+                   [ IsCapCategory, IsList ] );
+
+
+DeclareOperationWithCache( "MonomorphismInInjectiveObject", [ IsCapCategoryObject  ] );
+
+DeclareOperation( "AddMonomorphismInInjectiveObject",
+                   [ IsCapCategory, IsFunction, IsInt ] );
+
+DeclareOperation( "AddMonomorphismInInjectiveObject",
+                   [ IsCapCategory, IsFunction ] );
+
+
+DeclareOperation( "AddMonomorphismInInjectiveObject",
+                   [ IsCapCategory, IsList, IsInt ] );
+
+DeclareOperation( "AddMonomorphismInInjectiveObject",
+                   [ IsCapCategory, IsList ] );
