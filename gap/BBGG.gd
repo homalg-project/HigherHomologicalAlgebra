37343e146c01211d3ab6a7c55218b728e6b817cc
Kamal Saleh <ksaleh87@yahoo.com>
2018-06-12 21:27:59 +0200

    Moving some methods from examples to the package

--- gap/BBGG.gd
+++ gap/BBGG.gd
@@ -13,12 +13,6 @@ DeclareAttribute( "AsPresentationMorphismInHomalg",  IsGradedLeftOrRightPresenta
 KeyDependentOperation( "GradedLeftPresentationGeneratedByHomogeneousPart", IsGradedLeftPresentation, IsInt, ReturnTrue );
 DeclareAttribute( "EmbeddingInSuperObject", IsGradedLeftPresentation);
 
-DeclareAttribute( "RFunctor", IsHomalgGradedRing );
-DeclareAttribute( "LFunctor", IsHomalgGradedRing );
-DeclareAttribute( "TateFunctor", IsHomalgGradedRing );
-DeclareAttribute( "TateFunctorForCochains", IsHomalgGradedRing );
-DeclareAttribute( "TateSequenceFunctor", IsHomalgGradedRing );
-
 DeclareAttribute( "CastelnuovoMumfordRegularity", IsCapCategoryObject );
 DeclareAttribute( "TateResolution",  IsGradedLeftOrRightPresentation  );
 DeclareAttribute( "TateResolution",  IsGradedLeftOrRightPresentationMorphism );
@@ -27,3 +21,12 @@ DeclareOperation( "DimensionOfTateCohomology", [ IsCochainComplex, IsInt, IsInt 
 
 KeyDependentOperation( "TwistFunctor", IsHomalgGradedRing, IsInt, ReturnTrue );
 DeclareOperation( "\[\]", [ IsGradedLeftOrRightPresentation, IsInt ] );
+
+KeyDependentOperation( "TwistedStructureBundle", IsHomalgGradedRing, IsInt, ReturnTrue );
+KeyDependentOperation( "TwistedCotangentBundle", IsHomalgGradedRing, IsInt, ReturnTrue );
+KeyDependentOperation( "KoszulSyzygyModule", IsHomalgGradedRing, IsInt, ReturnTrue );
+DeclareAttribute( "RFunctor", IsHomalgGradedRing );
+DeclareAttribute( "LFunctor", IsHomalgGradedRing );
+DeclareAttribute( "TateFunctor", IsHomalgGradedRing );
+DeclareAttribute( "TateFunctorForCochains", IsHomalgGradedRing );
+DeclareAttribute( "TateSequenceFunctor", IsHomalgGradedRing );
