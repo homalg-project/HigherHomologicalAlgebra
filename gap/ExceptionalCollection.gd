7a85a7b67b875b80c34016470e96ab64aaa6a2f9
Kamal Saleh <ksaleh87@yahoo.com>
2020-01-24 09:57:12 +0100

    rename many full subcategories

--- gap/ExceptionalCollection.gd
+++ gap/ExceptionalCollection.gd
@@ -215,24 +215,4 @@ DeclareAttribute( "FullSubcategoryGeneratedByIndecInjectiveObjects",
 DeclareAttribute( "FullSubcategoryGeneratedByIndecProjRepresentationsOverOppositeAlgebra",
         IsAlgebroid );
 
-##################################
-##
-## Global function for display text
-##
-##################################
-
-if not IsBound( RANDOM_TEXT_ATTR ) then
-  
-  ##
-  DeclareGlobalFunction( "RANDOM_TEXT_ATTR" );
-  
-  ##
-  InstallGlobalFunction( RANDOM_TEXT_ATTR,
-    function (  )
-      return [ Random( [ "\033[31m", "\033[32m", "\033[33m", "\033[34m", "\033[35m",
-                #" \033[41m", " \033[42m", " \033[43m", " \033[44m", " \033[45m", " \033[46m", " \033[4m"
-                ] ), "\033[0m" ];
-  end );
-  
-fi;
 
