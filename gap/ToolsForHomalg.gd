9dcf76b6a5a9a5d77a4f83ee64f0b1801359663d
Kamal Saleh <ksaleh87@yahoo.com>
2020-04-23 00:36:05 +0200

    move code of monomials of given degree to GradedRingForHomalg package
    commit: ad993412c37

--- gap/ToolsForHomalg.gd
+++ gap/ToolsForHomalg.gd
@@ -3,6 +3,5 @@ DeclareGlobalFunction( "ENHANCE_HOMALG_GRADED_RING_WITH_RANDOM_FUNCTIONS" );
 DeclareAttribute( "HomalgElementToListOfIntegers", IsHomalgElement );
 DeclareAttribute( "CoefficientsOfGradedRingElement", IsHomalgGradedRingElement );
 DeclareAttribute( "MonomialsOfGradedRingElement", IsHomalgGradedRingElement );
-DeclareOperation( "MonomialsOfDegree", [ IsHomalgGradedRing, IsHomalgModuleElement ] );
 DeclareAttribute( "EntriesOfHomalgMatrixAttr", IsHomalgMatrix );
 DeclareAttribute( "EntriesOfHomalgMatrixAsListListAttr", IsHomalgMatrix );
