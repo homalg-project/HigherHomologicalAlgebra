325e8b14f32dd9f3b16bdda0d7518db5cebb8aab
Kamal Saleh <ksaleh87@yahoo.com>
2020-04-23 23:22:31 +0200

    move some methods for graded rows and Freyd categories

--- gap/ToolsForFreydCategories.gd
+++ gap/ToolsForFreydCategories.gd
@@ -9,15 +9,6 @@ DeclareGlobalFunction( "IS_FREYD_CATEGORY_OBJECT_OF_COX_RING_OF_PPS_IN_KERNEL_OF
 DeclareGlobalFunction( "ANNIHILATOR_OF_FREYD_CATEGORY_OBJECT_OF_COX_RING_OF_PPS" );
 DeclareAttribute( "UnderlyingCategoryOfRows", IsCategoryOfGradedRows );
 
-DeclareOperation( "\/", [ IsList, IsCategoryOfGradedRows ] );
-DeclareOperation( "\/", [ IsFreydCategoryObject, IsCapCategory ] );
-DeclareOperation( "\/", [ IsFreydCategoryMorphism, IsCapCategory ] );
-DeclareOperation( "\/", [ IsGradedLeftPresentation, IsFreydCategory ] );
-#DeclareOperation( "\/", [ IsGradedLeftPresentationMorphism, IsFreydCategory ] );
-DeclareOperation( "\/", [ IsGradedLeftPresentation, IsCategoryOfGradedRows ] );
-DeclareOperation( "\/", [ IsGradedLeftPresentationMorphism, IsCategoryOfGradedRows ] );
-DeclareOperation( "\/", [ IsGradedRow, IsFreydCategory ] );
-
 DeclareOperation( "BoxProduct", [ IsList, IsCapCategory ] );
 DeclareOperation( "BoxProduct",
       [ IsCapFullSubcategoryGeneratedByFiniteNumberOfObjects, IsCapFullSubcategoryGeneratedByFiniteNumberOfObjects, IsCapCategory ]
