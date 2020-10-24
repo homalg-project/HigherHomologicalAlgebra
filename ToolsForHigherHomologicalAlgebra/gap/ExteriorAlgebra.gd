0b7dab3aa47bd6c4601a7eb7e111a774f41e9802
Kamal Saleh <ksaleh87@yahoo.com>
2020-10-24 11:01:33 +0200

    remove some files since their content will be distributed in other packages or files

--- ToolsForHigherHomologicalAlgebra/gap/ExteriorAlgebra.gd
+++
@@ -1,9 +0,0 @@
-# SPDX-License-Identifier: GPL-2.0-or-later
-# ToolsForHigherHomologicalAlgebra: Tools for the Higher Homological Algebra project
-#
-# Declarations
-#
-DeclareAttribute( "IndicesForBasisOfExteriorAlgebra", IsHomalgRing );
-
-DeclareOperation( "SolveTwoSidedEquationOverExteriorAlgebra",
-        [ IsHomalgMatrix, IsHomalgMatrix, IsHomalgMatrix ] );
