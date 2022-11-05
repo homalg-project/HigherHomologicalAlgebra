8266fa6533911c1a5b416f1209721b2f29867aff
Kamal Saleh <kamal.saleh@web.de>
2022-11-05 01:23:42 +0100

    cleaning

--- ToolsForHigherHomologicalAlgebra/gap/MatricesForHomalg.gd
+++
@@ -1,8 +0,0 @@
-# SPDX-License-Identifier: GPL-2.0-or-later
-# ToolsForHigherHomologicalAlgebra: Tools for the Higher Homological Algebra project
-#
-# Declarations
-#
-
-DeclareAttribute( "EntriesOfHomalgMatrixAttr", IsHomalgMatrix );
-DeclareAttribute( "EntriesOfHomalgMatrixAsListListAttr", IsHomalgMatrix );
