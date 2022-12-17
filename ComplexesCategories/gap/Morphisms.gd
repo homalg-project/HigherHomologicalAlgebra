04863332eca28fcb8777f1be9944154df979b13c
Kamal Saleh <kamal.saleh@web.de>
2022-12-17 20:25:16 +0100

    Document ComplexesCategories

--- ComplexesCategories/gap/Morphisms.gd
+++ ComplexesCategories/gap/Morphisms.gd
@@ -2,37 +2,159 @@
 
 
 
+
+
+#! @Chapter Constructors
+
+#! @Section Constructing morphisms
+
+#! @Description
+#!  GAP-categories of the complex morphisms.
+#! @Arguments phi
 DeclareCategory( "IsChainOrCochainMorphism", IsCapCategoryMorphism );
+
+#! @Description
+#!  GAP-categories of the chain complex morphisms.
+#! @Arguments phi
 DeclareCategory( "IsChainMorphism", IsChainOrCochainMorphism );
+
+#! @Description
+#!  GAP-categories of the cochain complex morphisms.
+#! @Arguments phi
 DeclareCategory( "IsCochainMorphism", IsChainOrCochainMorphism );
 
-DeclareAttribute( "LowerBound", IsChainOrCochainMorphism );
-DeclareAttribute( "UpperBound", IsChainOrCochainMorphism );
-DeclareAttribute( "LowerBoundOfSourceAndRange", IsChainOrCochainMorphism );
-DeclareAttribute( "UpperBoundOfSourceAndRange", IsChainOrCochainMorphism );
+#! @Description
+#!  The input is a complexes category $\mathcal{C}^b(A)$ by chains or cochains, two objects $S$ and $R$ and a list $L$ with $3$ entries:
+#!  $L[1]$ is a $\mathbb{Z}$-function; $L[2]$ and $L[3]$ are integers or $\pm\infty$.
+#!  The output is the morphism $\phi:S \to R$ in $\mathcal{C}^b(A)$ whose morphism at $i\in\mathbb{Z}$ is $L[1][i]$
+#!  and its lower and upper bounds are $L[2]$ resp. $L[3]$.
+#! @Arguments C, S, L, R
+#! @Returns a CAP morphism
+DeclareOperation( "CreateComplexMorphism", [ IsComplexesCategory, IsChainOrCochainComplex, IsList, IsChainOrCochainComplex ] );
 
+#! @Description
+#!  The input is a complexes category $\mathcal{C}^b(A)$ by chains or cochains, two objects $S$ and $R$ and a dense-list $L$ of morphisms in $A$ and an integer $\ell$.
+#!  The output is the morphism $\phi:S \to R$ in $\mathcal{C}^b(A)$ whose morphism at $\ell$ is $L[1]$, at $\ell+1$ is $L[2]$, etc.
+#!  In particular, $\ell$ is a lower bound of $\phi$.
+#! @Arguments C, S, L, ell, R
+#! @Returns a CAP morphism
+DeclareOperation( "CreateComplexMorphism", [ IsComplexesCategory, IsChainOrCochainComplex, IsDenseList, IsInt, IsChainOrCochainComplex ] );
+
+#! @Description
+#!  Returns the morphisms as a $\mathbb{Z}$-function.
+#! @Arguments phi
+#! @Returns a $\mathbb{Z}$-function
 DeclareAttribute( "Morphisms", IsChainOrCochainMorphism );
 
+#! @Description
+#! Returns the morphism of $\phi$ at the index $i\in\mathbb{Z}$.
+#! @Arguments phi, i
+#! @Returns a CAP morphism
 KeyDependentOperation( "MorphismAt", IsChainOrCochainMorphism, IsInt, ReturnTrue );
 
-KeyDependentOperation( "CocyclesFunctorialAt", IsCochainMorphism, IsInt, ReturnTrue );
-KeyDependentOperation( "CohomologyFunctorialAt", IsCochainMorphism, IsInt, ReturnTrue );
-
-KeyDependentOperation( "CyclesFunctorialAt", IsChainMorphism, IsInt, ReturnTrue );
-KeyDependentOperation( "HomologyFunctorialAt", IsChainMorphism, IsInt, ReturnTrue );
-
+#! @Description
+#! Delegates to <C>MorphismAt</C>($\phi$, $i$).
+#! @Arguments phi, i
+#! @Returns a CAP morphism
 DeclareOperation( "\[\]", [ IsChainOrCochainMorphism, IsInt ] );
 
-DeclareAttribute( "MorphismsSupport", IsChainOrCochainMorphism );
+#! @Description
+#! Returns the lower bound $\ell$ of $\phi$. I.e., the morphisms $\phi^i$ are zero for all $i\prec\ell$.
+#! @Arguments phi
+#! @Returns integer or infinity
+DeclareAttribute( "LowerBound", IsChainOrCochainMorphism );
+
+#! @Description
+#! Returns the upper bound $u$ of $\phi$. I.e., the morphisms $\phi^i$ are zero for all $i\succ u$.
+#! @Arguments phi
+#! @Returns integer or infinity
+DeclareAttribute( "UpperBound", IsChainOrCochainMorphism );
+
+#! @Description
+#! Returns an integer $\ell$ with $S^i=R^i=0$ for all $i\prec\ell$.
+#! @Arguments phi
+#! @Returns integer or infinity
+DeclareAttribute( "LowerBoundOfSourceAndRange", IsChainOrCochainMorphism );
+
+#! @Description
+#! Returns an integer $u$ with $S^i=R^i=0$ for all $i\succ u$.
+#! @Arguments phi
+#! @Returns integer or infinity
+DeclareAttribute( "UpperBoundOfSourceAndRange", IsChainOrCochainMorphism );
+
+#! @Description
+#! The input is a complex morphism $\phi$ and two integers $m,n$.
+#! The output is the list of indices $m\preceq i \preceq n$ where the morphisms of $\phi$ are non-zero.
+#! @Arguments phi, m, n
+#! @Returns a list of integers
 DeclareOperation( "MorphismsSupport", [ IsChainOrCochainMorphism, IsInt, IsInt ] );
 
+#! @Description
+#! The input is a complex morphism $\phi$ whose lower and upper bounds are integers.
+#! The output is the list of indices where the morphisms of $\phi$ are non-zero.
+#! @Arguments phi, i
+#! @Returns a list of integers
+DeclareAttribute( "MorphismsSupport", IsChainOrCochainMorphism );
+
+#! @Description
+#! The input is a complex morphism $\phi:S \to R$ and an integer $i$.
+#! The output is the morphism induced by the functoriality of the cycle objects of $S$ and $R$ at the index $i$.
+#! @Arguments phi, i
+#! @Returns a CAP morphism
+KeyDependentOperation( "CyclesFunctorialAt", IsChainMorphism, IsInt, ReturnTrue );
+
+#! @Description
+#! The input is a complex morphism $\phi:S \to R$ and an integer $i$.
+#! The output is the morphism induced by the functoriality of the cocycle objects of $S$ and $R$ at the index $i$.
+#! @Arguments phi, i
+#! @Returns a CAP morphism
+KeyDependentOperation( "CocyclesFunctorialAt", IsCochainMorphism, IsInt, ReturnTrue );
+
+#! @Description
+#! The input is a complex morphism $\phi:S \to R$ and an integer $i$.
+#! The output is the morphism induced by the functoriality of the cohomology objects of $S$ and $R$ at the index $i$.
+#! @Arguments phi, i
+#! @Returns a CAP morphism
+KeyDependentOperation( "CohomologyFunctorialAt", IsCochainMorphism, IsInt, ReturnTrue );
+
+#! @Description
+#! The input is a complex morphism $\phi:S \to R$ and an integer $i$.
+#! The output is the morphism induced by the functoriality of the homology objects of $S$ and $R$ at the index $i$.
+#! @Arguments phi, i
+#! @Returns a CAP morphism
+KeyDependentOperation( "HomologyFunctorialAt", IsChainMorphism, IsInt, ReturnTrue );
+
+#! @Description
+#! Returns wheather $\phi$ is a quasi-isomorphism, i.e., wheather it induces isomorphisms between the (co)homology objects of $S$ and $R$.
+#! @Arguments phi
+#! @Returns true or false
 DeclareProperty( "IsQuasiIsomorphism", IsChainOrCochainMorphism );
+
+#! @Description
+#! Returns wheather $\phi$ is homotopic to the zero morphism.
+#! @Arguments phi
+#! @Returns true or false
 DeclareProperty( "IsHomotopicToZeroMorphism", IsChainOrCochainMorphism );
+
+#! @Description
+#! Returns a $\mathbb{Z}$-function $w$ witnessing that $\phi$ is homotopic to the zero morphism.
+#! If $\phi$ is a chain morphism, then $w^i$ is a morphism from $S^i$ to $R^{i+1}$ and
+#! if $\phi$ is a cochain morphism, then $w^i$ is a morphism from $S^i$ to $R^{i-1}$.
+#! @Arguments phi
+#! @Returns $\mathbb{Z}$-function
 DeclareAttribute( "WitnessForBeingHomotopicToZeroMorphism", IsChainOrCochainMorphism );
 
-DeclareOperation( "CreateComplexMorphism", [ IsComplexesCategory, IsChainOrCochainComplex, IsChainOrCochainComplex, IsList ] );
 DeclareAttribute( "AsComplexMorphismOverOppositeCategory", IsChainOrCochainMorphism );
 
+#! @Description
+#! Convert a chain morphism into a cochain morphism.
+#! @Arguments phi
+#! @Returns a CAP morphism
 DeclareAttribute( "AsChainComplexMorphism", IsCochainMorphism );
+
+#! @Description
+#! Convert a cochain morphism into a chain morphism.
+#! @Arguments phi
+#! @Returns a CAP morphism
 DeclareAttribute( "AsCochainComplexMorphism", IsChainMorphism );
 
--- ComplexesCategories/gap/Objects.gd
+++ ComplexesCategories/gap/Objects.gd
@@ -1,54 +1,246 @@
 
 
 
+
+
+
+#! @Chapter Constructors
+
+#! @Section Constructing objects
+
+#! @Description
+#!  GAP-categories of the chain or cochain complexes.
+#! @Arguments C
 DeclareCategory( "IsChainOrCochainComplex", IsCapCategoryObject );
+
+#! @Description
+#!  GAP-categories of the chain complexes.
+#! @Arguments C
 DeclareCategory( "IsChainComplex", IsChainOrCochainComplex );
+
+#! @Description
+#!  GAP-categories of the cochain complexes.
+#! @Arguments C
 DeclareCategory( "IsCochainComplex", IsChainOrCochainComplex );
 
-
+#! @Description
+#!  The input is a complexes category $\mathcal{C}^b(A)$ by chains or cochains and a list $L$ with $4$ entries:
+#!  $L[1]$ and $L[2]$ are $\mathbb{Z}$-functions and $L[3]$ and $L[4]$ are integers or $\pm\infty$.
+#!  The output is an object  $C\in \mathcal{C}^b(A)$ whose object at $i\in\mathbb{Z}$ is $C^i:=L[1][i]$, differential at $i\in\mathbb{Z}$ is
+#!  $\partial_{C}^i:=L[2][i]$. Its lower and upper bounds are $L[3]$ resp. $L[4]$.
+#! @Arguments C, L
+#! @Returns a CAP object
 DeclareOperation( "CreateComplex", [ IsComplexesCategory, IsList ] );
 
-DeclareAttribute( "Differentials", IsChainOrCochainComplex );
-DeclareAttribute( "Objects", IsChainOrCochainComplex );
-DeclareAttribute( "UpperBound", IsChainOrCochainComplex );
-DeclareAttribute( "LowerBound", IsChainOrCochainComplex );
+#! @Description
+#!  The input is a complexes category $\mathcal{C}^b(A)$ by chains or cochains, a dense-list $L$ of morphisms in $A$ and an integer $\ell$.
+#!  The output is an object $C\in\mathcal{C}^b(A)$ whose differentials are $\partial_{C}^{\ell}:=L[1], \partial_{C}^{\ell+1}:=L[2],$ etc.
+#! @Arguments C, L
+#! @Returns a CAP object
+DeclareOperation( "CreateComplex", [ IsComplexesCategoryByCochains, IsDenseList, IsInt ] );
 
+#! @Description
+#!  Returns the objects of the complex as a $\mathbb{Z}$-function.
+#! @Arguments C
+#! @Returns a $\mathbb{Z}$-function
+DeclareAttribute( "Objects", IsChainOrCochainComplex );
+
+#! @Description
+#! Returns the object $C^i$ of the complex $C$ at the index $i\in\mathbb{Z}$.
+#! @Arguments C, i
+#! @Returns a CAP object
 KeyDependentOperation( "ObjectAt", IsChainOrCochainComplex, IsInt, ReturnTrue );
+
+#! @Description
+#! Delegates to <C>ObjectAt</C>($C$,$i$).
+#! @Arguments C, i
+#! @Returns a CAP object
 DeclareOperation( "\[\]", [ IsChainOrCochainComplex, IsInt ] );
 
+#! @Description
+#! Returns the differentials of the complex as a $\mathbb{Z}$-function.
+#! @Arguments C
+#! @Returns a $\mathbb{Z}$-function
+DeclareAttribute( "Differentials", IsChainOrCochainComplex );
+
+#! @Description
+#! Returns the differential of the complex $C$ at the index $i\in\mathbb{Z}$.
+#! @Arguments C, i
+#! @Returns a CAP morphism
 KeyDependentOperation( "DifferentialAt", IsChainOrCochainComplex, IsInt, ReturnTrue );
+
+#! @Description
+#! Delegates to <C>DifferentialAt</C>($C$,$i$).
+#! @Arguments C, i
+#! @Returns a CAP object
 DeclareOperation( "\^", [ IsChainOrCochainComplex, IsInt ] );
 
+#! @Description
+#! Returns the lower bound $\ell$ of $C$. I.e., the objects $C^i$ are zero for all $i\prec\ell$.
+#! @Arguments C
+#! @Returns integer or infinity
+DeclareAttribute( "LowerBound", IsChainOrCochainComplex );
+
+#! @Description
+#! Returns the upper bound $u$ of $C$. I.e., the objects $C^i$ are zero for all $i\succ u$.
+#! @Arguments C
+#! @Returns integer or infinity
+DeclareAttribute( "UpperBound", IsChainOrCochainComplex );
+
+#! @Description
+#! The input is a complex $C$ and two integers $m,n$.
+#! The output is the list of indices $m\preceq i \preceq n$ where the objects of $C$ are non-zero.
+#! @Arguments C, m, n
+#! @Returns a list of integers
 DeclareOperation( "ObjectsSupport", [ IsChainOrCochainComplex, IsInt, IsInt ] );
+
+#! @Description
+#! The input is a complex $C$ whose lower and upper bounds are integers.
+#! The output is the list of indices where the objects of $C$ are non-zero.
+#! @Arguments C
+#! @Returns a list of integers
 DeclareAttribute( "ObjectsSupport", IsChainOrCochainComplex );
 
+#! @Description
+#! The input is a complex $C$ and two integers $m,n$.
+#! The output is the list of indices $m\preceq i \preceq n$ where the differentials of $C$ are non-zero.
+#! @Arguments C, m, n
+#! @Returns a list of integers
 DeclareOperation( "DifferentialsSupport", [ IsChainOrCochainComplex, IsInt, IsInt ] );
+
+#! @Description
+#! The input is a complex $C$ whose lower and upper bounds are integers.
+#! The output is the list of indices where the differentials of $C$ are non-zero.
+#! @Arguments C
+#! @Returns a list of integers
 DeclareAttribute( "DifferentialsSupport", IsChainOrCochainComplex );
 
+#! @Description
+#! The input is a cochain complex $C$.
+#! The output is the kernel object of $\partial_{C}^i$.
+#! @Arguments C, i
+#! @Returns a CAP object
 KeyDependentOperation( "CocyclesAt", IsCochainComplex, IsInt, ReturnTrue );
+
+#! @Description
+#! The input is a cochain complex $C$.
+#! The output is the kernel embedding of $\partial_{C}^i$.
+#! @Arguments C, i
+#! @Returns a CAP object
 KeyDependentOperation( "CocyclesEmbeddingAt", IsCochainComplex, IsInt, ReturnTrue );
+
+#! @Description
+#! The input is a cochain complex $C$.
+#! The output is the image object of the differential $\partial_{C}^{i-1}$.
+#! @Arguments C, i
+#! @Returns a CAP object
 KeyDependentOperation( "CoboundariesAt", IsCochainComplex, IsInt, ReturnTrue );
+
+#! @Description
+#! The input is a cochain complex $C$.
+#! The output is the image embedding of the differential $\partial_{C}^{i-1}$.
+#! @Arguments C, i
+#! @Returns a CAP morphism
 KeyDependentOperation( "CoboundariesEmbeddingAt", IsCochainComplex, IsInt, ReturnTrue );
+
+#! @Description
+#! The input is a cochain complex $C$.
+#! The output is the cohomology object of $C$ at $i$.
+#! @Arguments C, i
+#! @Returns a CAP object
 KeyDependentOperation( "CohomologyAt", IsCochainComplex, IsInt, ReturnTrue );
+
+#! @Description
+#! The input is a complex $C$ and two integers $m,n$.
+#! The output is the list of indices $m\preceq i \preceq n$ where the cohomology objects of $C$ are non-zero.
+#! @Arguments C, m, n
+#! @Returns a list of integers
 DeclareOperation( "CohomologySupport", [ IsCochainComplex, IsInt, IsInt ] );
+
+#! @Description
+#! The input is a complex $C$ whose lower and upper bounds are integers.
+#! The output is the list of indices where the cohomology objects of $C$ are non-zero.
+#! @Arguments C
+#! @Returns a list of integers
 DeclareAttribute( "CohomologySupport", IsCochainComplex );
 
+#! @Description
+#! The input is a cochain complex $C$.
+#! The output is the kernel object of $\partial_{C}^i$.
+#! @Arguments C, i
+#! @Returns a CAP object
 KeyDependentOperation( "CyclesAt", IsChainComplex, IsInt, ReturnTrue );
+
+#! @Description
+#! The input is a cochain complex $C$.
+#! The output is the kernel embedding of $\partial_{C}^i$.
+#! @Arguments C, i
+#! @Returns a CAP object
 KeyDependentOperation( "CyclesEmbeddingAt", IsChainComplex, IsInt, ReturnTrue );
+
+#! @Description
+#! The input is a cochain complex $C$.
+#! The output is the image object of the differential $\partial_{C}^{i+1}$.
+#! @Arguments C, i
+#! @Returns a CAP object
 KeyDependentOperation( "BoundariesAt", IsChainComplex, IsInt, ReturnTrue );
+
+#! @Description
+#! The input is a cochain complex $C$.
+#! The output is the image embedding of the differential $\partial_{C}^{i+1}$.
+#! @Arguments C, i
+#! @Returns a CAP morphism
 KeyDependentOperation( "BoundariesEmbeddingAt", IsChainComplex, IsInt, ReturnTrue );
+
+#! @Description
+#! The input is a cochain complex $C$.
+#! The output is the homology object of $C$ at $i$.
+#! @Arguments C, i
+#! @Returns a CAP object
 KeyDependentOperation( "HomologyAt", IsChainComplex, IsInt, ReturnTrue );
+
+#! @Description
+#! The input is a complex $C$ and two integers $m,n$.
+#! The output is the list of indices $m\preceq i \preceq n$ where the homology objects of $C$ are non-zero.
+#! @Arguments C, m, n
+#! @Returns a list of integers
 DeclareOperation( "HomologySupport", [ IsChainComplex, IsInt, IsInt ] );
+
+#! @Description
+#! The input is a complex $C$ whose lower and upper bounds are integers.
+#! The output is the list of indices where the homology objects of $C$ are non-zero.
+#! @Arguments C
+#! @Returns a list of integers
 DeclareAttribute( "HomologySupport", IsChainComplex );
 
+#! @Description
+#! The input is a complex $C$ whose lower and upper bounds are integers.
+#! The output is wheather the (co)homology support is empty.
+#! @Arguments C
+#! @Returns a list of integers
 DeclareProperty( "IsExact", IsChainOrCochainComplex );
+
+#! @Description
+#! The input is a complex $C$ and two integers $m,n$.
+#! The output is wheather the (co)homology support between $m$ and $n$ is empty.
+#! @Arguments C, m, n
+#! @Returns a list of integers
 DeclareOperation( "IsExact", [ IsChainOrCochainComplex, IsInt, IsInt ] );
 
 DeclareAttribute( "AsComplexOverOppositeCategory", IsCochainComplex );
 
+#! @Description
+#! Convert a cochain complex into a chain complex.
+#! @Arguments C
+#! @Returns a CAP object
 DeclareAttribute( "AsChainComplex", IsCochainComplex );
+
+#! @Description
+#! Convert a chain complex into a cochain complex.
+#! @Arguments C
+#! @Returns a CAP object
 DeclareAttribute( "AsCochainComplex", IsChainComplex );
 
-BindGlobal( "ChainComplex", ReturnNothing );
-BindGlobal( "CochainComplex", ReturnNothing );
 
+DeclareSynonym( "ChainComplex", CreateComplex );
+DeclareSynonym( "CochainComplex", CreateComplex );
