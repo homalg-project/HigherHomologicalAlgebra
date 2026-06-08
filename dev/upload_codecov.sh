1747f927aa23ccfbc9cab27580ea3103c61fed00
Kamal Saleh <kamal.saleh@web.de>
2026-06-08 12:13:01 +0200

    Apply PackageJanitor

--- dev/upload_codecov.sh
+++ dev/upload_codecov.sh
@@ -3,7 +3,7 @@
 set -e
 
 # get GPG key
-curl -O https://keybase.io/codecovsecurity/pgp_keys.asc
+curl -O https://keybase.io/codecovsecops/pgp_keys.asc
 # verify fingerprint
 if ! gpg --import --import-options show-only --with-fingerprint pgp_keys.asc | grep "2703 4E7F DB85 0E0B BC2C  62FF 806B B28A ED77 9869"; then
     echo "Downloaded GPG key has wrong fingerprint"
@@ -13,9 +13,9 @@ fi
 gpg --no-default-keyring --keyring ~/.gnupg/trustedkeys.kbx --import pgp_keys.asc
 
 # get uploader with signatures
-curl -O https://uploader.codecov.io/latest/linux/codecov
-curl -O https://uploader.codecov.io/latest/linux/codecov.SHA256SUM
-curl -O https://uploader.codecov.io/latest/linux/codecov.SHA256SUM.sig
+curl -O https://cli.codecov.io/latest/linux/codecov
+curl -O https://cli.codecov.io/latest/linux/codecov.SHA256SUM
+curl -O https://cli.codecov.io/latest/linux/codecov.SHA256SUM.sig
 
 # verify
 gpgv codecov.SHA256SUM.sig codecov.SHA256SUM
